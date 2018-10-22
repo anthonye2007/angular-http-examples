ecad19dd709338ab42eb827caa5d04967a890785
Anthony Elliott <aelliott@pillartechnology.com>
2018-10-22 11:13:54 -0500

    Make POST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ class Todo {
   title: string;
 }
 
+class Post {
+  id: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,10 +19,18 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
   title = 'sample-post';
   todos = null;
+  created = new Post();
 
   makeGet() {
-    console.log('here');
+    console.log('in GET');
     const url = 'https://jsonplaceholder.typicode.com/todos/1';
     this.http.get<Todo>(url).subscribe(data => this.todos = data.title);
+  }
+
+  makePost() {
+    console.log('in POST');
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const body = {};
+    this.http.post<Post>(url, body).subscribe(data => this.created = data);
   }
 }
