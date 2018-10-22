f1caa8a65bf20c8b81a3209b1a978e30b7265a57
Anthony Elliott <aelliott@pillartechnology.com>
2018-10-22 11:05:51 -0500

    Make GET request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {}
   title = 'sample-post';
+  todos = null;
 
-  makePost() {
+  makeGet() {
     console.log('here');
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    this.http.get(url).subscribe(data => this.todos = data.title);
   }
 }
